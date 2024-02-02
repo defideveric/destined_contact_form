dfc2796ac1eb4778c8c3e98bb5518e99b5217d50
erichudson <defideveric@gmail.com>
2024-02-02 17:27:26 -0500

    adjustments to form, message section added

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -30,9 +30,11 @@ export default function Home() {
 
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
+    <main className="flex min-h-screen flex-col items-center justify-between p-20 bg-white">
       <Image src="/DESTINED Logo-Navy.png" alt="logo" width={500} height={300}/>
-        <h1 className="text-gray-500 pb-6 text-center">Please provide your contact info and donation load size.</h1>
+        <h1 className="text-gray-500 pb-4 text-center">Please provide your contact info and donation load size.</h1>
+          <p className="mb-6 text-xs font-bold text-center text-blue-900">Note: We only accept gently worn items. Bug and mold free. <br/> Please contact us prior to requesting a driver if you have any questions.</p>
+
         <form class="max-w-md mx-auto" ref={form} onSubmit={sendEmail}>
           <div class="relative z-0 w-full mb-5 group">
               <input type="name" name="name" id="name" class="block py-2.5 px-10 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
@@ -65,9 +67,15 @@ export default function Home() {
                 <input type="text" name="company" id="company" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                 <label for="floating_company" class="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Company (Ex. Google)</label>
             </div>
+
+            <div class="relative z-0 w-full mb-5 group">
+                <input type="text" name="message" id="message" class="block py-2.5 px-5 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                <label for="floating_message" class="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Additional Details</label>
+            </div>
           </div>
-          <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
+          <button type="submit" class="text-white bg-blue-900 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
         </form>
+
     </main>
   );
 }
